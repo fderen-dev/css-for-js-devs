25dc7d7f25ba233ee937490f91699234f5b9b9bf
fderen-dev <fderen.dev@gmail.com>
2024-01-20 17:15:33 +0100

    Exercise 3 - shell styling

--- sole-and-ankle/src/components/ShoeIndex/ShoeIndex.js
+++ sole-and-ankle/src/components/ShoeIndex/ShoeIndex.js
@@ -25,7 +25,7 @@ const ShoeIndex = ({ sortId, setSortId }) => {
           </Select>
         </Header>
         <Spacer size={34} />
-        <ShoeGrid />
+        {/* <ShoeGrid /> */}
       </MainColumn>
       <LeftColumn>
         <Breadcrumbs>
@@ -42,17 +42,32 @@ const ShoeIndex = ({ sortId, setSortId }) => {
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+display: flex;
+flex-direction: row-reverse;
+column-gap: 32px;
+`;
 
-const LeftColumn = styled.div``;
+const LeftColumn = styled.div`
+flex: 1;
+`;
 
-const MainColumn = styled.div``;
+const MainColumn = styled.div`
+flex: 3;
+display: flex;
+height: min-content;
+`;
 
-const Header = styled.header``;
+const Header = styled.header`
+display: flex;
+align-items: center;
+flex: 1;
+`;
 
 const Title = styled.h2`
   font-size: 1.5rem;
   font-weight: ${WEIGHTS.medium};
+  margin-inline-end: auto;
 `;
 
 export default ShoeIndex;
